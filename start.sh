e1bbb8c82b2f8202aba491f6d2e5833f6ca6bc29
sandrine <107451923+sandrine542@users.noreply.github.com>
2022-11-22 05:23:48 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=c98e4039-9765-4341-8af2-76d2f387f361
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=www.gameshedge.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
